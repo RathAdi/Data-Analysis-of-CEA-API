9eb25b3bdf05aaa5e680d715d1a0c8451f39aa81
RathAdi <hartstorm93@gmail.com>
2023-12-18 13:42:13 +0530

    Added duplicate row filtering

--- bcr.py
+++ bcr.py
@@ -7,6 +7,7 @@ source = pd.read_json("https://cea.nic.in/api/installed_capacity.php")
 source = source[["Region","Month","Total"]]
 
 source = source.pivot(index = "Month",columns= "Region", values = "Total")
+source = source.drop_duplicates()
 
 source = source.dropna()
 
