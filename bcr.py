3f4e0277b06ab2ae071099ab58988df30e9f2544
RathAdi <hartstorm93@gmail.com>
2023-12-18 14:11:02 +0530

    Reconverted sorted date format to readable strings

--- bcr.py
+++ bcr.py
@@ -2,6 +2,7 @@ import bar_chart_race as bcr
 import pandas as pd
 from datetime import datetime
 
+
 source = pd.read_json("https://cea.nic.in/api/installed_capacity.php")
 
 source = source[["Region","Month","Total"]]
@@ -11,13 +12,19 @@ source = source.drop_duplicates()
 
 source = source.dropna()
 
-df = pd.DataFrame(columns=["Northern","Eastern","Western","Southern","North Eastern"])
+sorted_source = pd.DataFrame(columns=["Northern","Eastern","Western","Southern","North Eastern"])
 
 for index,row in source.iterrows():
     new_index = datetime.strptime(index, "%b-%Y")
-    df.loc[new_index] = [row["Northern"],row["Eastern"],row["Western"],row["Southern"],row["North Eastern"]]
+    sorted_source.loc[new_index] = [row["Northern"],row["Eastern"],row["Western"],row["Southern"],row["North Eastern"]]
 
-df = df.sort_index()
+sorted_source = sorted_source.sort_index()
+
+df = pd.DataFrame(columns=["Northern","Eastern","Western","Southern","North Eastern"])
+
+for index,row in sorted_source.iterrows():
+    new_index = datetime.strftime(index, "%b-%Y")
+    df.loc[new_index] = [row["Northern"],row["Eastern"],row["Western"],row["Southern"],row["North Eastern"]]
 
 bcr.bar_chart_race(
     df = df,
